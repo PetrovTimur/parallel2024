b216f1eb87138678750178d67a425aa9ef67224c
Timur Petrov <s02240520@gse.cs.msu.ru>
2024-10-21 12:04:40 +0300

    Add solve

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <tuple>
 #include <vector>
 #include <cmath>
+#include <complex>
+#include <omp.h>
 
 std::tuple<int, int> input(int Nx, int Ny, int K1, int K2) {
     std::cout << "Hello, World!" << std::endl;
@@ -61,29 +63,34 @@ void makeCSR(int Nx, int Ny, int K1, int K2, std::vector<int> &ia, std::vector<i
     ia[(Nx + 1) * (Ny + 1)] = k;
 }
 
-void fillCSR(std::vector<int> &ia, std::vector<int> &ja, std::vector<double> &a, std::vector<double> &b) {
+void fillCSR(std::vector<int> &ia, std::vector<int> &ja, std::vector<double> &a, std::vector<double> &b, std::vector<double> &diag) {
+    #pragma omp parallel for
     for (int i = 0; i < ia.size() - 1; i++) {
         double sum = 0;
-        int k_i;
+        int k_i = 0;
         for (int k = ia[i]; k < ia[i + 1]; k++) {
             int j = ja[k];
             if (i != j) {
                 a[k] = std::cos(i * j + i + j);
-                sum += a[k];
+                sum += std::abs(a[k]);
             } else
                 k_i = k;
-            std::cout << "el: " << i << ", j: " << j << ", val = " << a[k] << std::endl;
+            // std::cout << "el: " << i << ", j: " << j << ", val = " << a[k] << std::endl;
         }
 
         a[k_i] = 1.234 * sum;
-        std::cout << "el: " << i << ", j: " << ja[k_i] << ", val = " << a[k_i] << std::endl;
+        diag[i] = a[k_i];
+        // std::cout << "el: " << i << ", j: " << ja[k_i] << ", val = " << a[k_i] << std::endl;
     }
+
+    for (int i = 0; i < b.size(); i++)
+        b[i] = std::sin(i);
 }
 
 double dot(std::vector<double> &x, std::vector<double> &y) {
     double sum = 0;
 
-    // #pragma omp parallel for reduction(+:sum)
+    #pragma omp parallel for reduction(+:sum)
     for (int i = 0; i < x.size(); i++) {
         sum += x[i] * y[i];
     }
@@ -91,11 +98,75 @@ double dot(std::vector<double> &x, std::vector<double> &y) {
     return sum;
 }
 
-void solve() {
+void spMV(std::vector<int> &ia, std::vector<int> &ja, std::vector<double> &a, std::vector<double> &b, std::vector<double> &res) {
+    #pragma omp parallel for
+    for (int i = 0; i < ia.size() - 1; i++) {
+        double sum = 0;
+        for (int col = ia[i]; col < ia[i + 1]; col++) {
+            int j = ja[col];
+            double a_ij = a[col];
+            sum += a_ij * b[j];
+        }
+        res[i] = sum;
+    }
+}
 
+void solve(std::vector<int> &ia, std::vector<int> &ja, std::vector<double> &a, std::vector<double> &b, std::vector<double> &diag, std::vector<double> &res) {
+    int N = ia.size() - 1;
+    double eps = 0.001;
+    int maxit = 10;
+
+    std::vector<double> x(N);
+    std::vector<double> r(b);
+    std::vector<double> z(N);
+    std::vector<double> p(N);
+    std::vector<double> q(N);
+    std::vector<double> rho(2);
+    int k = 0;
+
+    do {
+        k++;
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++) {
+            z[i] = r[i] / diag[i];
+        }
+
+
+        rho[0] = rho[1];
+        rho[1] = dot(r, z);
+        if (k == 1)
+            #pragma omp parallel for
+            for (int i = 0; i < N; i++)
+                p[i] = z[i];
+        else {
+            double beta = rho[1] / rho[0];
+            #pragma omp parallel for
+            for (int i = 0; i < N; i++)
+                p[i] = z[i] + beta * p[i];
+        }
+
+        spMV(ia, ja, a, p, q);
+        double alpha = rho[1] / dot(p, q);
+
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++)
+            x[i] = x[i] + alpha * p[i];
+
+        #pragma omp parallel for
+        for (int i = 0; i < N; i++)
+            r[i] = r[i] - alpha * p[i];
+    }
+    while (rho[1] > eps * eps && k < maxit);
+
+    #pragma omp parallel for
+    for (int i = 0; i < N; i++)
+        res[i] = x[i];
 }
 
 int main(int argc, char** argv) {
+    double start = omp_get_wtime();
+
+    omp_set_num_threads(12);
 
     int Nx = std::stoi(argv[1]);
     int Ny = std::stoi(argv[2]);
@@ -110,9 +181,25 @@ int main(int argc, char** argv) {
     std::vector<int> ja(nonzero_elements);
     std::vector<double> a(nonzero_elements);
     std::vector<double> b(nodes);
+    std::vector<double> diag(nodes);
 
     makeCSR(Nx, Ny, K1, K2, ia, ja);
-    fillCSR(ia, ja, a, b);
+    fillCSR(ia, ja, a, b, diag);
+
+    std::vector<double> res(nodes);
+
+    solve(ia, ja, a, b, diag, res);
+
+    double end = omp_get_wtime();
+    printf("Work took %f seconds\n", end - start);
+
+    /*for (int i = 0; i < nodes; i++) {
+        std::cout << res[i] << " ";
+    }*/
+
+    std::cout << res[253] << std::endl;
+
+    // std::cout << std::endl;
 
     return 0;
 }
