b4afa07e728e70abf3eefa2c0f3b15a3843be014
Timur Petrov <s02240520@gse.cs.msu.ru>
2024-11-04 18:00:37 +0300

    Add CGSolver measurements

---
+++ Tests/Time/CGSolverMeasure.cpp
@@ -0,0 +1,57 @@
+#include <omp.h>
+#include <iostream>
+#include <ostream>
+#include <tuple>
+
+#include "Solver/csr.h"
+#include "Solver/solvers.h"
+#include "Utilities/input.h"
+
+int main() {
+    omp_set_num_threads(omp_get_max_threads());
+    std::vector<int> ia;
+    std::vector<int> ja;
+    std::vector<double> a;
+    std::vector<double> b;
+    std::vector<double> diag;
+    std::vector<double> res;
+
+    std::cout << "T = " << omp_get_max_threads() << std::endl;
+
+    for (int k = 1e4; k <= 1e7; k *= 10) {
+        int K1 = 30;
+        int K2 = 23;
+        int Nx = 1000;
+        int Ny = k / Nx;
+        std::tuple<int, int> t = input(Nx, Ny, K1, K2);
+        int nodes = std::get<0>(t);
+        int nonzero_elements =std::get<1>(t);
+
+        ia.resize(nodes + 1);
+        ja.resize(nonzero_elements);
+        a.resize(nonzero_elements);
+        b.resize(nodes);
+        diag.resize(nodes);
+        res.resize(nodes);
+
+        makeCSR(Nx, Ny, K1, K2, ia, ja);
+        fillCSR(ia, ja, a, b, diag);
+
+        int runs = 2 * omp_get_max_threads() * (1e4 / k + 1);
+        int iterations = 0;
+        double aggregate_time = 0;
+        for (int p = 0; p < runs; ++p) {
+            // Calculate
+            double start = omp_get_wtime();
+            iterations = solve(ia, ja, a, b, diag, res);
+            double end = omp_get_wtime();
+
+            aggregate_time += end - start;
+        }
+
+        double average_time = aggregate_time / runs;
+
+        std::cout << iterations * (3 * nodes + 3 * nodes + a.size()) / (average_time * 1e9) << ", ";
+    }
+    std::cout << std::endl;
+}
