9f958fd98a2086b942e5e9436074dd0a12235057
Timur Petrov <s02240520@gse.cs.msu.ru>
2024-11-05 09:25:07 +0300

    Add thread binding

--- Solver/Kernels/mathfunc.cpp
+++ Solver/Kernels/mathfunc.cpp
@@ -6,7 +6,7 @@
 void dot(std::vector<double> &x, std::vector<double> &y, double &res) {
     double sum = 0;
 
-    #pragma omp parallel for reduction(+:sum)
+    #pragma omp parallel for reduction(+:sum) proc_bind(spread)
     for (int i = 0; i < x.size(); i++) {
         sum += x[i] * y[i];
     }
@@ -15,7 +15,7 @@ void dot(std::vector<double> &x, std::vector<double> &y, double &res) {
 
 void spMV(std::vector<int> &ia, std::vector<int> &ja, std::vector<double> &a, std::vector<double> &b,
     std::vector<double> &res) {
-    #pragma omp parallel for
+    #pragma omp parallel for proc_bind(spread)
     for (int i = 0; i < ia.size() - 1; i++) {
         double sum = 0;
         for (int col = ia[i]; col < ia[i + 1]; col++) {
@@ -28,21 +28,21 @@ void spMV(std::vector<int> &ia, std::vector<int> &ja, std::vector<double> &a, st
 }
 
 void axpy(double a, std::vector<double> &x, std::vector<double> &y, std::vector<double> &res) {
-    #pragma omp parallel for
+    #pragma omp parallel for proc_bind(spread)
     for (int i = 0; i < x.size(); i++) {
         res[i] = a * x[i] + y[i];
     }
 }
 
 void vecCopy(std::vector<double> &x, std::vector<double> &y) {
-    #pragma omp parallel for
+    #pragma omp parallel for proc_bind(spread)
     for (int i = 0; i < x.size(); i++) {
         y[i] = x[i];
     }
 }
 
 void vecInit(std::vector<double> &x, const double a) {
-    #pragma omp parallel for
+    #pragma omp parallel for proc_bind(spread)
     for (int i = 0; i < x.size(); i++) {
         x[i] = a;
     }
