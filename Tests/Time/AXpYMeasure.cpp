a966f0fb5290a54bf55a54ca2b54fdc6a539f13a
Timur Petrov <s02240520@gse.cs.msu.ru>
2024-12-11 03:26:23 +0300

    Add MPI support to AXpYMeasure test for parallel execution

--- Tests/Time/AXpYMeasure.cpp
+++ Tests/Time/AXpYMeasure.cpp
@@ -7,7 +7,82 @@
 
 #include "Solver/Kernels/mathfunc.h"
 
-int main() {
+#ifdef USE_MPI
+#include <mpi.h>
+#endif
+
+int main(int argc, char **argv) {
+    #ifdef USE_MPI
+    // omp_set_num_threads(omp_get_max_threads());
+
+    std::vector<double> x;
+    std::vector<double> y;
+    std::vector<double> res;
+
+    int mpi_res;
+    mpi_res = MPI_Init(&argc, &argv); // первым делом подключаемся к MPI
+    if(mpi_res != MPI_SUCCESS) {
+        MPI_Abort(MPI_COMM_WORLD, -1);
+        exit(mpi_res);
+    }
+
+    int NumProc;
+    mpi_res = MPI_Comm_size(MPI_COMM_WORLD,&NumProc); // узнаем число процессов
+    if(mpi_res!= MPI_SUCCESS){
+        MPI_Abort(MPI_COMM_WORLD, -1);
+        exit(mpi_res);
+    }
+
+    int MyID;
+    mpi_res = MPI_Comm_rank(MPI_COMM_WORLD,&MyID); // узнаем номер данного процесса
+    if(mpi_res!= MPI_SUCCESS){
+        MPI_Abort(MPI_COMM_WORLD, -1);
+        exit(mpi_res);
+    }
+
+    if (MyID == 0)
+        std::cout << "NumProc = " << NumProc << std::endl;
+
+    // int T  = omp_get_max_threads();
+    // std::cout << "T = " << T << std::endl;
+
+    for (int k = 1e5; k <= 1e8; k *= 10) {
+        x.resize(k / NumProc);
+        y.resize(k / NumProc);
+        res.resize(k / NumProc);
+
+        // Fill
+        // #pragma omp parallel for proc_bind(spread)
+        for (int i = 0; i < k / NumProc; i++) {
+            x[i] = sin(MyID * k / NumProc + i);
+            y[i] = cos(MyID * k / NumProc + i);
+        }
+
+        int runs = 1e9 / k + 1;
+        double aggregate_time = 0;
+        for (int p = 1; p < runs + 1; ++p) {
+            double alpha = std::tan(p);
+            // Calculate
+            double start = MPI_Wtime();
+            axpy(alpha, x, y, res);
+            MPI_Barrier(MPI_COMM_WORLD);
+            double end = MPI_Wtime();
+
+            aggregate_time += end - start;
+
+        }
+
+        double average_time = aggregate_time / runs;
+
+        if (MyID == 0)
+            std::cout << 2 * k / (average_time * 1e9) << ", ";
+    }
+    if (MyID == 0)
+        std::cout << std::endl;
+
+    MPI_Finalize();
+
+    #else
     omp_set_num_threads(omp_get_max_threads());
 
     std::vector<double> x;
@@ -47,4 +122,7 @@ int main() {
         std::cout << 2 * k / (average_time * 1e9) << ", ";
     }
     std::cout << std::endl;
+    #endif
+
+    return 0;
 }
