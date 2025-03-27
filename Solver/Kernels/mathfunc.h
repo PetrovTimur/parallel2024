c8627612bcba560091af5df95ae53a17f7910a68
Timur Petrov <s02240520@gse.cs.msu.ru>
2025-03-27 22:41:23 +0300

    Add documentation for arrCopy and arrInit functions

--- Solver/Kernels/mathfunc.h
+++ Solver/Kernels/mathfunc.h
@@ -12,6 +12,14 @@ void spMV(const int *ia, const int *ja, const double *a, const double *b, int si
 
 void axpy(double a, const double *x, const double *y, int size, double *res);
 
+/**
+ * @brief Copy one array data to another array
+ *
+ * @tparam T data type
+ * @param y where to copy
+ * @param x where to copy from
+ * @param size array size
+ */
 template <typename T>
 void arrCopy(T *y, const T *x, const int size) {
     #pragma omp parallel for default(none) shared(x, y, size)
@@ -20,6 +28,15 @@ void arrCopy(T *y, const T *x, const int size) {
     }
 }
 
+
+/**
+ * @brief Initialize array with some value
+ *
+ * @tparam T data type
+ * @param x array to initialize
+ * @param a initialization value
+ * @param size array size
+ */
 template <typename T>
 void arrInit(T *x, const T a, const int size) {
     #pragma omp parallel for default(none) shared(x, a, size)
