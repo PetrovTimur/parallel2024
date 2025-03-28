dd7f0cd4dc3cd566410b4ef0187bc04cc1b040d9
Timur Petrov <s02240520@gse.cs.msu.ru>
2025-03-28 13:57:04 +0300

    Update arrCopy and arrInit functions to use unsigned long for size parameter

--- Solver/Kernels/mathfunc.h
+++ Solver/Kernels/mathfunc.h
@@ -1,6 +1,5 @@
 #ifndef MATHFUNC_H
 #define MATHFUNC_H
-#include <vector>
 
 void dot(const double *x, const double *y, int size, double &res);
 
@@ -21,7 +20,7 @@ void axpy(double a, const double *x, const double *y, int size, double *res);
  * @param size array size
  */
 template <typename T>
-void arrCopy(T *y, const T *x, const int size) {
+void arrCopy(T *y, const T *x, const unsigned long size) {
     #pragma omp parallel for default(none) shared(x, y, size)
     for (int i = 0; i < size; i++) {
         y[i] = x[i];
@@ -38,7 +37,7 @@ void arrCopy(T *y, const T *x, const int size) {
  * @param size array size
  */
 template <typename T>
-void arrInit(T *x, const T a, const int size) {
+void arrInit(T *x, const T a, const unsigned size) {
     #pragma omp parallel for default(none) shared(x, a, size)
     for (int i = 0; i < size; i++) {
         x[i] = a;
