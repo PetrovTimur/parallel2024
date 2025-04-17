971ed5eb36eb31de954d7722939c868f02052ff2
Timur Petrov <s02240520@gse.cs.msu.ru>
2025-04-17 20:46:48 +0300

    Update DotCUDATest

--- Tests/DotCUDATest.cu
+++ Tests/DotCUDATest.cu
@@ -9,28 +9,26 @@ int main() {
     int blocks = 128;
     int threads = 256;
     int size = 100000000;
-    float a = 2.0f;
+    float a = 2.f;
+    float b = 3.f;
 
     thrust::device_vector<float> d_x(size, a);
-    thrust::device_vector<float> d_y(size, a);
-    thrust::device_vector<float> d_z(blocks, a);
+    thrust::device_vector<float> d_y(size, b);
+    thrust::device_vector<float> d_z(blocks);
 
     float res_host;
     float *res;
     cudaMalloc(&res, sizeof(float));
-
-    // dot<<<blocks, threads, threads*sizeof(float)>>>(d_x.data().get(), d_y.data().get(), d_z.data().get(), size);
-    // reduce0<<<1, blocks, blocks*sizeof(float)>>>(d_z.data().get(), res, blocks);
 
     dot_gpu(threads, blocks, d_x.data().get(), d_y.data().get(), d_z.data().get(), res, size);
 
     cudaMemcpy(&res_host, res, sizeof(float), cudaMemcpyDeviceToHost);
 
     // Verify the result & print first error if any
-    bool correct = (res_host == a * size * a);
+    bool correct = (res_host == a * b * size);
 
     std::cout << res_host << std::endl;
-    std::cout << 2 * size * a << std::endl;
+    std::cout << size * a * b << std::endl;
 
 
     if (correct) {
